594972a1c63f50b9c35ce36e2686487e8e166ceb
aryan <kaintharyan@gmail.com>
2022-01-19 18:40:55 +0530

    Minor Bug Fix and Sending Messages

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
+function MessageForm(props) {
+    const [value, setValue] = useState('');
+    const { chatId, creds } = props
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const text = value.trim();
+        if (text.length > 0) {
+            sendMessage(creds, chatId, { text }) //built in 
+        }
+        setValue('')
+    }
+    const handleChange = (e) => {
+        setValue(e.target.value)
+        isTyping(props, chatId)
 
-function MessageForm() {
-  return <div>
-      Form
-  </div>;
+    }
+    return <form className="message-form" onSubmit={handleSubmit}>
+        <input type="text" className="message-input" placeholder='Send a new Message' value={value} onChange={handleChange} onSubmit={handleSubmit} />
+    </form>
 }
 
 export default MessageForm;

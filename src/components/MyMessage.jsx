804c80c308f7329b446959d8f8c339a5205268a4
aryan <kaintharyan@gmail.com>
2022-01-19 17:33:23 +0530

    MyMessage Component

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-function MyMessage() {
-  return <div>
-      My Message
+function MyMessage({message}) {
+    if(message?.attachments?.length>0) //check if text or image 
+    {
+        return (
+            <img src ={message.attachments[0].file} className='message-image' style={{float:'right'}}/>
+
+        )
+    }
+  return <div className='message' style={{float:'right',marginRight:'18px',color:'white',backgroundColor:'#3B2A50'}}> 
+      {message.text}
   </div>;
 }
 

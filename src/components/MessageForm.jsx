6f7634950cb498b0a865b7a50022a3d1f96296e8
aryan <kaintharyan@gmail.com>
2022-01-19 18:58:24 +0530

    sending Images

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react'
 import { sendMessage, isTyping } from 'react-chat-engine'
+import { SendOutlined, PictureOutlined } from '@ant-design/icons'
 function MessageForm(props) {
     const [value, setValue] = useState('');
     const { chatId, creds } = props
@@ -17,8 +18,21 @@ function MessageForm(props) {
         isTyping(props, chatId)
 
     }
+    const onUpload=(e)=>{
+        sendMessage(creds,chatId,{files:e.target.files,text:''})
+    }
     return <form className="message-form" onSubmit={handleSubmit}>
         <input type="text" className="message-input" placeholder='Send a new Message' value={value} onChange={handleChange} onSubmit={handleSubmit} />
+        <label htmlFor="upload-button" >
+            <span className="image-button">
+                <PictureOutlined className='pictur-icon' />
+            </span>
+            
+        </label>
+        <input type="file" multiple={false} id='upload-button' style={{display:'none'} } onChange={onUpload} />
+        <button className="send-button">
+            <SendOutlined  className='send-icon'/>
+        </button>
     </form>
 }
 

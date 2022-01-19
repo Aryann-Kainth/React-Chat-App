c5e8f8b1bb3f7493354f44abe394a9496fc8bb85
aryan <kaintharyan@gmail.com>
2022-01-19 17:24:08 +0530

    Chat Feed Work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+import './App.css'
+const App = () => {
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID="07d5e49a-a656-4411-a5fa-2f1765f20c3a"
+            userName="Aryan"
+            userSecret="123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
 }
-
-export default App;
+export default App

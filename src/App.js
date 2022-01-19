527f3f30f6b09688ece64bc6f8c43cf362fd589c
aryan <kaintharyan@gmail.com>
2022-01-19 19:34:28 +0530

    Final Changes Login etc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 import './App.css'
 const App = () => {
+    if(!localStorage.getItem('username'))
+    {
+        return <LoginForm />
+    }
     return (
         <ChatEngine
             height='100vh'
             projectID="07d5e49a-a656-4411-a5fa-2f1765f20c3a"
-            userName="Aryan"
-            userSecret="123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
